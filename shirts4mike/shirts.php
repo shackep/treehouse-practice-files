e33330a2f0d8eeb568ed7b191304946f3a06d3e5
shackep <peter.shackelford@gmail.com>
2014-07-02 21:00:49 -0400

    "Added Products array"

--- shirts4mike/shirts.php
+++ shirts4mike/shirts.php
@@ -1,11 +1,25 @@
-<?php 
+<?php
+$products = array();
+$products[] = "Log Shirt, Red";
+$products[] = "Mike the Frog Shirt, Black";
+$products[] = "Mike the Frog Shirt, Blue";
+$products[] = "Logo Shirt, Green";
+
+?><?php 
 $pageTitle = "Mike's Full Catalog of Shirts";
 $section = "shirts";
 include('inc/header.php'); ?>
 
 		<div class="section page">
+			<div class="wrapper">
 
 			<h1>Mike&rsquo;s Full Catalog of Shirts</h1>
+
+			<ul>
+				<?php foreach($products as $product) {?> 
+				<li><?php echo $product; ?></li>
+				<?php} ?>
+			</ul>
 		</div>
 
 
