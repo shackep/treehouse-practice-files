487b88f35ab2fbfdd148f678b25c813ee5ab7090
shackep <peter.shackelford@gmail.com>
2014-07-09 09:36:00 -0400

    finished lesson on arrays

--- shirts4mike/shirts.php
+++ shirts4mike/shirts.php
@@ -1,24 +1,66 @@
 <?php
 $products = array();
-$products[101] = "Log Shirt, Red";
-$products[102] = "Mike the Frog Shirt, Black";
-$products[103] = "Mike the Frog Shirt, Blue";
-$products[104] = "Logo Shirt, Green";
-Here we have some text that does not belong!
+
+$products[101] = array(
+	"name" => "Logo Shirt, Red",
+	"price" => 18,
+	"img" => "img/shirts/shirt-101.jpg"
+	);
+
+$products[102] = array(
+	"name" => "Mike the Frog Shirt, Black",
+	"price" => 20,
+	"img" => "img/shirts/shirt-102.jpg"
+	);
+
+$products[103] = array(
+	"name" => "Mike the Frog Shirt, Blue",
+	"price" => 25,
+	"img" => "img/shirts/shirt-103.jpg"
+	);
+
+$products[104] = array(
+	"name" => "Logo Shirt, Green",
+	"price" => 25,
+	"img" => "img/shirts/shirt-104.jpg"
+	);
+
+$products[106] = array(
+	"name" => "Logo Shirt, Gray",
+	"price" => 20,
+	"img" => "img/shirts/shirt-106.jpg"
+	);
+
+$products[107] = array(
+	"name" => "Logo Shirt, Turquoise",
+	"price" => 20,
+	"img" => "img/shirts/shirt-107.jpg"
+	);
+
+$products[108] = array(
+	"name" => "Logo Shirt, Orange",
+	"price" => 20,
+	"img" => "img/shirts/shirt-108.jpg"
+	);
 ?><?php
 $pageTitle = "Mike's Full Catalog of Shirts";
 $section = "shirts";
 include('inc/header.php'); ?>
 
-		<div class="section page">
+		<div class="section shirtspage">
 			<div class="wrapper">
-
 			<h1>Mike&rsquo;s Full Catalog of Shirts</h1>
 
-			<ul>
-				<?php foreach($products as $product) {?> 
-				<li><?php echo $product; ?></li>
-				<?php} ?>
+			<ul class="products">
+				<?php foreach($products as $product) {
+					echo "<li>";
+					echo '<a href="#">';
+					echo '<img src="' . $product["img"] .'"alt="'. $product["name"]'">'
+					echo "<p>View Details</p>";
+					echo "</a>";
+					echo "</li>";
+					}
+			?>
 			</ul>
 		</div>
 
